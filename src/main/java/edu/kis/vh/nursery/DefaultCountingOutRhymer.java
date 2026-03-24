e59654ca55cbbfc93c212cc61f843da36c67b321
Szymon Goszczyk <257475@edu.p.lodz.pl>
2026-03-24 21:41:30 +0100

    12. Usunięcie nieużywanego settera

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -13,10 +13,6 @@ public class DefaultCountingOutRhymer {
         return total;
     }
 
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
     private int total = EMPTY;
 
     public void countIn(int in) {
