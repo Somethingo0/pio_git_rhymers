4305a25e6ee702cfbb986d00b06913a0d8b1a7f5
Szymon Goszczyk <257475@edu.p.lodz.pl>
2026-04-07 17:46:50 +0200

    17. Dodanie dokumentacji Javadoc

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa bazowa wyliczanki.
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int CAPACITY = 12;
@@ -14,12 +17,18 @@ public class DefaultCountingOutRhymer {
     }
 
     private int total = EMPTY;
-
+    /**
+     * Dodaje element do wyliczanki.
+     * * @param in wartość, która zostanie dodana do stosu wyliczanki
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
-
+    /**
+     * Sprawdza, czy wyliczanka jest pusta.
+     * * @return true jeśli jest pusta, false w przeciwnym razie
+     */
     public boolean callCheck() {
         return total == EMPTY;
     }
@@ -34,6 +43,10 @@ public class DefaultCountingOutRhymer {
         return numbers[total];
     }
 
+    /**
+     * Zwraca i usuwa ostatni element z wyliczanki.
+     * * @return wartość ostatniego elementu lub -1 (RETURN_DEFAULT), gdy wyliczanka jest pusta
+     */
     public int countOut() {
         if (callCheck())
             return RETURN_DEFAULT;
